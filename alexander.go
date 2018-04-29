d22162edf1a8b61b94fe407b8235f2983ac5f66a
tddhit <tdandang@163.com>
2018-04-29 11:09:15 +0800

    增加统计

--- alexander.go
+++ alexander.go
@@ -2,22 +2,94 @@ package alexander
 
 import (
 	"bufio"
+	"context"
 	"net/http"
 	neturl "net/url"
 	"os"
+	"time"
+
+	"github.com/tddhit/tools/log"
 )
 
-func Request(rawurl string, data []string, header http.Header, requestNum int, opt Option) error {
+const (
+	MAX_TIME_DURATION = 1 * time.Hour
+)
+
+type Stats struct {
+	max     int64
+	Min     int64
+	avg     int64
+	success int64
+	fail    int64
+	elapsed int64
+}
+
+func Summarize(stats []*Stats) {
+	var (
+		max     int64
+		min     int64 = int64(MAX_TIME_DURATION)
+		avg     int64
+		success int64
+		fail    int64
+		elapsed int64
+	)
+	for _, s := range stats {
+		if s.max > max {
+			max = s.max
+		} else if s.Min < min {
+			min = s.Min
+		}
+		success += s.success
+		fail += s.fail
+		elapsed += s.elapsed
+	}
+	avg = elapsed / success
+
+	log.Info("\t\t\tSummary:")
+	log.Infof("\t\t\tmax:\t%dms", max/1000000)
+	log.Infof("\t\t\tmin:\t%dms", min/1000000)
+	log.Infof("\t\t\tavg:\t%dms", avg/1000000)
+	log.Infof("\t\t\tfail:\t%d", fail)
+	log.Infof("\t\t\tsucc:\t%d", success)
+}
+
+func Request(ctx context.Context, rawurl string, data []string,
+	header http.Header, repeatTimes int, opt Option, stats *Stats) error {
+
 	url, err := neturl.Parse(rawurl)
 	if err != nil {
 		return err
 	}
 	c := NewClient(opt, url.Host)
-	for i := 0; i < requestNum; i++ {
+	for i := 0; i < repeatTimes; i++ {
 		for j, _ := range data {
-			c.Request("POST", url.Path, header, []byte(data[j]))
+			select {
+			case <-ctx.Done():
+				goto exit
+			default:
+				start := time.Now()
+				_, err := c.Request("POST", url.Path, header, []byte(data[j]))
+				end := time.Now()
+				elapsed := end.Sub(start)
+
+				if int64(elapsed) > stats.max {
+					stats.max = int64(elapsed)
+				} else if int64(elapsed) < stats.Min {
+					stats.Min = int64(elapsed)
+				}
+
+				if err != nil {
+					log.Error(err, elapsed)
+					stats.fail++
+				} else {
+					log.Info(elapsed)
+					stats.elapsed += int64(elapsed)
+					stats.success++
+				}
+			}
 		}
 	}
+exit:
 	return nil
 }
 
--- apps/alexander/alexander.go
+++ apps/alexander/alexander.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
 	"sync"
 	"time"
 
 	"github.com/tddhit/alexander"
+	"github.com/tddhit/tools/log"
 )
 
 var (
 	url            string
 	postFile       string
-	requestNum     int
+	repeatTimes    int
 	concurrency    int
 	connectTimeout int
 	readTimeout    int
@@ -22,7 +26,7 @@ var (
 func init() {
 	flag.StringVar(&url, "url", "", "url")
 	flag.StringVar(&postFile, "p", "", "post file")
-	flag.IntVar(&requestNum, "n", 1, "the number of requests")
+	flag.IntVar(&repeatTimes, "n", 1, "repeat n times")
 	flag.IntVar(&concurrency, "c", 1, "concurrency")
 	flag.IntVar(&connectTimeout, "ct", 1000, "connect timeout")
 	flag.IntVar(&readTimeout, "rt", 3000, "read timeout")
@@ -43,15 +47,38 @@ func main() {
 		KeepAlive:       30 * time.Second,
 		MaxIdleConns:    10,
 	}
-
 	header := make(http.Header)
 	header.Add("Content-Type", "application/json")
 
-	var wg sync.WaitGroup
+	var (
+		stats       []*alexander.Stats
+		wg          sync.WaitGroup
+		ctx, cancel = context.WithCancel(context.Background())
+	)
+
 	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		s := &alexander.Stats{
+			Min: int64(alexander.MAX_TIME_DURATION),
+		}
+		stats = append(stats, s)
+		go func(s *alexander.Stats) {
+			alexander.Request(ctx, url, data, header, repeatTimes, opt, s)
+			wg.Done()
+		}(s)
+	}
+
 	go func() {
-		alexander.Request(url, data, header, requestNum, opt)
-		wg.Done()
+		log.Error(http.ListenAndServe(":6062", nil))
 	}()
+
+	go func() {
+		signalChan := make(chan os.Signal)
+		signal.Notify(signalChan)
+		<-signalChan
+		cancel()
+	}()
+
 	wg.Wait()
+	alexander.Summarize(stats)
 }
